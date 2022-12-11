dcdfb14eab0390eed4b52d7cef28591207e471ac
Gustavo Êrades Vilarinho Silva <eradesvilarinho@gmail.com>
2022-12-11 15:56:25 -0300

    alterações e novo arquivo

--- home_pos_Login.php
+++ home_pos_Login.php
@@ -8,6 +8,7 @@ if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)
     unset($_SESSION['senha']);
     header('Location:login.php');
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
