9be8685a7759cf800b8670a179509539f92a2251
Gustavo Êrades Vilarinho Silva <eradesvilarinho@gmail.com>
2022-12-15 00:04:32 -0300

    alterar depois as partes do back

--- bd_coach.sql
+++ bd_coach.sql
@@ -27,6 +27,9 @@ ALTER TABLE `db_coach`.`cadastro`
 CHANGE COLUMN `telefone` `telefone` VARCHAR(20) NULL DEFAULT NULL ;
 
 
+select*from db_coach.cadastro;
+show tables
+
 
 
 
--- show_sistema_persona.php
+++ show_sistema_persona.php
@@ -1,169 +1,167 @@
 <?php
-<<<<<<< HEAD
-    session_start();
-    include_once('config.php');
-  
-    //cadastro
-    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
-    {
-        unset($_SESSION['email']);
-        unset($_SESSION['senha']);
-        header('Location:entrar.php');
-    }
-    $logado = $_SESSION['email'];
-    if(!empty($_GET['search']))
-    {
-      $data=$_GET['search'];
-      $sql= "SELECT * FROM cadastro WHERE cod LIKE '%$data%' or nome LIKE '%$data%' or email LIKE '%$data%' or 
-      telefone LIKE '%$data%' or sexo LIKE '%$data%' or senha LIKE '%$data%' ";
-  
-    }
-    else
-    {
-      $sql = "SELECT*FROM cadastro ORDER BY cod DESC";
-    }
-  
-    
-    $result2 = $conexao_regis->query($sql);
-  
-  
-  if(isset($_POST['submit']))
-  {
-  
-=======
-  session_start();
->>>>>>> f92f60bb7d66c4a0fb88c84947d69a1c6c07c26a
-  include_once('config.php');
-
-  //cadastro
-  if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
-  {
-      unset($_SESSION['email']);
-      unset($_SESSION['senha']);
-      header('Location:entrar.php');
-  }
-<<<<<<< HEAD
-
-?>
-=======
-  $logado = $_SESSION['email'];
-  if(!empty($_GET['search']))
-  {
-    $data=$_GET['search'];
-    $sql= "SELECT * FROM cadastro WHERE cod LIKE '%$data%' or nome LIKE '%$data%' or email LIKE '%$data%' or 
-    telefone LIKE '%$data%' or sexo LIKE '%$data%' or senha LIKE '%$data%' ";
-
-  }
-  else
-  {
-    $sql = "SELECT*FROM cadastro ORDER BY cod DESC";
-  }
-
-  
-  $result2 = $conexao_regis->query($sql);
-
-
-if(isset($_POST['submit']))
-{
-
+session_start();
 include_once('config.php');
 
-$nome= $_POST['username'];
-$email= $_POST['email'];
-$senha= $_POST['password'];
-$telefone= $_POST['phone'];
-$sexo= $_POST['sexo'];
+//cadastro
+if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+  unset($_SESSION['email']);
+  unset($_SESSION['senha']);
+  header('Location:entrar.php');
+}
+$logado = $_SESSION['email'];
+if (!empty($_GET['search'])) {
+  $data = $_GET['search'];
+  $sql = "SELECT * FROM cadastro WHERE cod LIKE '%$data%' or nome LIKE '%$data%' or email LIKE '%$data%' or 
+    telefone LIKE '%$data%' or sexo LIKE '%$data%' or senha LIKE '%$data%' ";
 
-$result= mysqli_query($conexao_regis, "INSERT INTO cadastro(nome,email,senha,telefone,sexo) 
-VALUES ('$nome','$email','$senha','$telefone','$sexo')");
+} else {
+  $sql = "SELECT * FROM cadastro ORDER BY cod DESC";
 }
 
 
- ?>
+$result2 = $conexao_regis->query($sql);
 
->>>>>>> f92f60bb7d66c4a0fb88c84947d69a1c6c07c26a
+
+if (isset($_POST['submit'])) {
+
+  include_once('config.php');
+
+  $nome = $_POST['username'];
+  $email = $_POST['email'];
+  $senha = $_POST['password'];
+  $telefone = $_POST['phone'];
+  $sexo = $_POST['sexo'];
+
+  $result = mysqli_query($conexao_regis, "INSERT INTO cadastro(nome,email,senha,telefone,sexo) 
+VALUES ('$nome','$email','$senha','$telefone','$sexo')");
+}
+//testes
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width= , initial-scale=1.0">
-    <title>sistema</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style>
-      .box-search{
-        display: flex;
-        justify-content: center;
-        gap: .1%;
-      }
-    </style>
-  </head>
+  <meta charset="UTF-8">
+  <title>Sistema</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="shortcut icon" href="assets/images/favico.png" type="image/x-icon">
+  
+</head>
 <body>
-    <h1>Acessou o sistema</h1>
-    <div class="d-flex">
-        <a href="sair.php">sair</a>
+<!-- partial:index.partial.html -->
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>sistema</title>
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.teal-deep_purple.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Roboto:300,300i,400,400i,500,700,900" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header administration">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <div class="current-user">
+            <i class="material-icons">account_circle</i>
+            <?php echo "olá,$logado!" ?>
+          </div>
+          <div class="mdl-layout-spacer"></div>
+          
+          <div class="box-search" >
+      <input type="search" class="form-control w-25" placeholder="pesquisar" id="pesquisar">
+      <button class="btn btn-success" onclick="searchData()">
+        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-search'
+          viewBox='0 0 16 16'>
+          <path
+            d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+        </svg>
+      </button>
     </div>
-    <br>
-    
-    <?php 
-        echo "<h1>Bem Vindo <u>$logado</u> :) </h1>";
-    ?>
-   <br>
-<!-- cadastro -->
-<div class="m-5">
-    <h1>Cadastro</h1>
-<table class="table">
-  <thead class="thead-dark">
-    <tr>
-    <th scope="col">código</th>
-    <th scope="col">nome</th>
-      <th scope="col">Email</th>
-      <th scope="col">telefone</th>
-      <th scope="col">sexo</th>
-      <th scope="col">senha</th>
-      <th scope="col">...</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-      if($user_data=mysqli_fetch_assoc($result2))
-      {
-        echo "<tr>";
-        echo "<td>".$user_data['cod']."</td>";
-        echo "<td>".$user_data['nome']."</td>";
-        echo "<td>".$user_data['email']."</td>";
-        echo "<td>".$user_data['telefone']."</td>";
-        echo "<td>".$user_data['sexo']."</td>";
-        echo "<td>".$user_data['senha']."</td>";
-        echo "<td>
+          <!--
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+            <span>Procurar</span>
+            <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+              <i class="material-icons" onclick="searchData()">search</i>
+            </label>
+            <div class="mdl-textfield__expandable-holder">
+              <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp" id="pesquisar">
+            </div>
+-->
+      </header>
+      <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Mais opções</span>
+        <nav class="mdl-navigation">
+          <br>
+          <a class="mdl-navigation__link active" href="sair.php">sair</a>
+          <br>
+          <a class="mdl-navigation__link active" href="home_pos_login.php">Inicio</a>
+        </nav>
+      </div>
+      <main class="mdl-layout__content">
+        <div class="page-content">
+       
+
+          <div class="mdl-card mdl-shadow--2dp customer-list">
+            <table class="mdl-data-table mdl-js-data-table">
+              <thead>
+                <tr>
+                  <th class="mdl-data-table__cell__non-numeric">Código</th>
+                  <th class="mdl-data-table__cell__non-numeric">Nome</th>
+                  <th class="mdl-data-table__cell__non-numeric">Email</th>
+                  <th class="mdl-data-table__cell__non-numeric">Telefone</th>
+                  <th class="mdl-data-table__cell__non-numeric">Sexo</th>
+                  <th class="mdl-data-table__cell__non-numeric">Senha</th>
+                  <th>Editar</th>
+                  <th class="mdl-data-table__cell__non-numeric">Código endereço</th>
+                  <th class="mdl-data-table__cell__non-numeric">Cidade</th>
+                  <th class="mdl-data-table__cell__non-numeric">Endereço</th>
+                  <th class="mdl-data-table__cell__non-numeric">CEP</th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php
+        if ($user_data = mysqli_fetch_assoc($result2)) {
+          echo "<tr>";
+          echo "<td>" . $user_data['cod'] . "</td>";
+          echo "<td>" . $user_data['nome'] . "</td>";
+          echo "<td>" . $user_data['email'] . "</td>";
+          echo "<td>" . $user_data['telefone'] . "</td>";
+          echo "<td>" . $user_data['sexo'] . "</td>";
+          echo "<td>" . $user_data['senha'] . "</td>";
+          echo "<td>
           <a class='btn btn-sm btn-primary' href='edit_regis.php?cod=$user_data[cod]'>
           <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pen' viewBox='0 0 16 16'>
             <path d='m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z'/>
           </svg>
           </a>
 
-          <a class='btn btn-sm btn-danger' href='delete_conta.php?cod=$user_data[cod]'>
+          <a class='btn btn-sm btn-danger' href='delete.php?cod=$user_data[cod]'>
           <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>
             <path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'/>
             <path fill-rule='evenodd' d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'/>
           </svg>
           </a>
-          
-          <a  class='btn btn-sm btn-warning' href='show_sistema_forms.php?cod=$user_data[cod]'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-card-text' viewBox='0 0 16 16'>
-            <path d='M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z'/>
-            <path d='M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z'/>
-          </svg>
+          <a  class='btn btn-sm btn-warning' href='coach_show_sistema_forms.php?cod=$user_data[cod]'>
+          <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-card-text' viewBox='0 0 16 16'>
+          <path d='M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z'/>
+          <path d='M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z'/>
+        </svg>
           </a>
-<<<<<<< HEAD
-          </td>";
+        </td>";
           echo "<tr>";
         }
         ?>
               </tbody>
             </table>
+          </div>
+          <!-- content end -->
         </div>
       </main>
+    </div>
+
     <script>
       $(document).ready(function(){
 
@@ -173,28 +171,29 @@ VALUES ('$nome','$email','$senha','$telefone','$sexo')");
         });
 
       });
-      var search= document.getElementById('pesquisar');
-=======
-        </td>";
-        echo "<tr>";
-      }
-    ?>
-  </tbody>
-</table>
-</body>
-<script>
-  var search= document.getElementById('pesquisar');
->>>>>>> f92f60bb7d66c4a0fb88c84947d69a1c6c07c26a
-  search.addEventListener("keydown", function(event){
-    if(event.key === "Enter")
-    {
+      var search = document.getElementById('pesquisar');
+  search.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
       searchData();
     }
   });
-  function searchData()
-  {
-    window.location='show_sistema_persona.php?search='+search.value;
+  function searchData() {
+    window.location = 'sistema.php?search=' + search.value;
   }
 
-</script>
+  $(document).ready(function () {
+
+    $(".search-block").hide();
+    $(".expander-title").click(function () {
+      $(this).next(".search-block").slideToggle("fast");
+    });
+
+  });
+    </script>
+
+  </body>
+</html>
+<!-- partial -->
+  
+</body>
 </html>
