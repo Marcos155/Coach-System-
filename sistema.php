898b0e5da936dc12fcab40a7a5c0083e3b1a65f0
Gustavo Êrades Vilarinho Silva <eradesvilarinho@gmail.com>
2022-12-20 22:38:39 -0300

    ablu ble ble

--- sistema.php
+++ sistema.php
@@ -94,6 +94,8 @@ VALUES ('$nome','$email','$cidade','$estado','$telefone','$sexo')");
           <br>
           <a class="mdl-navigation__link active" href="sistema.php">Conta-Alunos</a>
           <br>
+          <a class="mdl-navigation__link" href="sistema_forms.php">Formulário-Alunos</a>
+          <br>
           <a class="mdl-navigation__link" href="coach_show_sistema_forms.php">Meta-Alunos</a>
           <br>
           <a class="mdl-navigation__link" href="sair.php">Sair</a>
