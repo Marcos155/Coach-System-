1802548153cf8a4fba0963fb89f3999017239477
Gustavo Êrades Vilarinho Silva <eradesvilarinho@gmail.com>
2022-12-30 23:49:02 -0300

    mudanças no back

--- bd_coach.sql
+++ bd_coach.sql
@@ -38,7 +38,70 @@ ALTER TABLE db_coach.formulario
 add column data_inicio date not null after desc_meta;
 ALTER TABLE db_coach.formulario
 add column email varchar (45) not null after nome;
-
-
-
+insert into db_coach.cadastro values("Gustavo","eradesvilarinho@gmail.com",123456,"Brasilia","DF",61994490664,"masculino",1);
+create table db_coach.formulario_15_anos(
+	saude varchar (500),
+    relacionamento varchar (500),
+    financeiro varchar (500),
+    espiritual varchar(500),
+    outro varchar (800),
+	cod int not null auto_increment,
+    foreign key (cod) references cadastro(cod),
+    primary key (cod)
+);
+create table db_coach.saude_12_meses(
+	oque varchar (300) not null,
+    porquem varchar (300) not null,
+    onde varchar (300) not null,
+    quando date not null,
+    porque varchar (300),
+    como varchar (300),
+	cod int not null auto_increment,
+    foreign key (cod) references cadastro(cod),
+    primary key (cod)
+);
+create table db_coach.relacionamento_12_meses(
+	oque varchar (300) not null,
+    porquem varchar (300) not null,
+    onde varchar (300) not null,
+    quando date not null,
+    porque varchar (300),
+    como varchar (300),
+	cod int not null auto_increment,
+    foreign key (cod) references cadastro(cod),
+    primary key (cod)
+);
+create table db_coach.trabalho_12_meses(
+	oque varchar (300) not null,
+    porquem varchar (300) not null,
+    onde varchar (300) not null,
+    quando date not null,
+    porque varchar (300),
+    como varchar (300),
+	cod int not null auto_increment,
+    foreign key (cod) references cadastro(cod),
+    primary key (cod)
+);
+create table db_coach.dinheiro_12_meses(
+	oque varchar (300) not null,
+    porquem varchar (300) not null,
+    onde varchar (300) not null,
+    quando date not null,
+    porque varchar (300),
+    como varchar (300),
+	cod int not null auto_increment,
+    foreign key (cod) references cadastro(cod),
+    primary key (cod)
+);
+create table db_coach.outro_12_meses(
+	oque varchar (300) not null,
+    porquem varchar (300) not null,
+    onde varchar (300) not null,
+    quando date not null,
+    porque varchar (300),
+    como varchar (300),
+	cod int not null auto_increment,
+    foreign key (cod) references cadastro(cod),
+    primary key (cod)
+);
 
