5d385f84ce1d8d9aa132e5f51d7f38899cc6a54e
Gustavo Êrades Vilarinho Silva <eradesvilarinho@gmail.com>
2023-01-02 20:34:20 -0300

    mudanças no banco

--- bd_coach.sql
+++ bd_coach.sql
@@ -7,14 +7,15 @@ create table cadastro(
 	cod int not null auto_increment, 
     primary key(cod,email)
 );
-create table formulario(
-    meta varchar (300) not null,
-    nome varchar (45) not null,
-    data_conclusao date not null,
-    status_meta varchar (500) not null,
-    cod int not null auto_increment,
+create table db_coach.formulario_15_anos(
+	saude varchar (500),
+    relacionamento varchar (500),
+    financeiro varchar (500),
+    espiritual varchar(500),
+    outro varchar (800),
+	cod int not null auto_increment,
     foreign key (cod) references cadastro(cod),
-    primary key(cod)
+    primary key (cod)
 );
 create table tb_login(
 	email varchar (45) not null,
@@ -32,23 +33,9 @@ ALTER TABLE db_coach.cadastro
 add column cidade varchar(50) null after senha;
 ALTER TABLE db_coach.cadastro
 add column estado varchar(40) null after cidade;
-ALTER TABLE db_coach.formulario
-add column desc_meta varchar(500) not null after meta;
-ALTER TABLE db_coach.formulario
-add column data_inicio date not null after desc_meta;
-ALTER TABLE db_coach.formulario
-add column email varchar (45) not null after nome;
+;
 insert into db_coach.cadastro values("Gustavo","eradesvilarinho@gmail.com",123456,"Brasilia","DF",61994490664,"masculino",1);
-create table db_coach.formulario_15_anos(
-	saude varchar (500),
-    relacionamento varchar (500),
-    financeiro varchar (500),
-    espiritual varchar(500),
-    outro varchar (800),
-	cod int not null auto_increment,
-    foreign key (cod) references cadastro(cod),
-    primary key (cod)
-);
+
 create table db_coach.saude_12_meses(
 	oque varchar (300) not null,
     porquem varchar (300) not null,
@@ -104,20 +91,9 @@ create table db_coach.outro_12_meses(
     foreign key (cod) references cadastro(cod),
     primary key (cod)
 );
-ALTER TABLE db_coach.formulario
-add column saude varchar(200) null after nome;
-ALTER TABLE db_coach.formulario
-add column relacionamento varchar(200) null after saude;
-ALTER TABLE db_coach.formulario
-add column financeiro varchar(200) null after relacionamento;
-ALTER TABLE db_coach.formulario
-add column espiritual varchar(200) null after financeiro;
-ALTER TABLE db_coach.formulario
-add column outro varchar(200) null after espiritual;
 ALTER TABLE db_coach.formulario_15_anos
 add column nome varchar(45) null after outro;
 ALTER TABLE db_coach.formulario_15_anos
 add column email varchar(45) null after nome;
 
-select*from db_coach.formulario_15_anos;
-show tables
+
