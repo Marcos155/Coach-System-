583e744f3ebc0beb0708062cd00f61ccf4f55537
Gustavo Êrades Vilarinho Silva <eradesvilarinho@gmail.com>
2023-01-16 19:31:18 -0300

    resolvendo bugs do delete

--- delete.php
+++ delete.php
@@ -4,8 +4,26 @@
     include_once('config.php');
 
     $cod = $_GET['cod'];
-    $sqlselect = "SELECT * FROM formulario_15_anos WHERE cod=$cod";
+    $sqlselect = "SELECT * FROM formulario_15_anos  WHERE cod=$cod";
     $result = $conexao_forms15->query($sqlselect);
+
+    $sqlselect2 = "SELECT * FROM saude_12_meses  WHERE cod=$cod";
+    $result2 = $conexao_forms15->query($sqlselect2);
+
+    $sqlselect3 = "SELECT * FROM relacionamento_12_meses  WHERE cod=$cod";
+    $result3 = $conexao_forms15->query($sqlselect3);
+
+    $sqlselect4= "SELECT * FROM trabalho_12_meses  WHERE cod=$cod";
+    $result4 = $conexao_forms15->query($sqlselect4);
+
+    $sqlselect5 = "SELECT * FROM dinheiro_12_meses  WHERE cod=$cod";
+    $result5 = $conexao_forms15->query($sqlselect5);
+
+    $sqlselect6 = "SELECT * FROM outro_12_meses  WHERE cod=$cod";
+    $result6 = $conexao_forms15->query($sqlselect6);
+
+    $sqlselect7 = "SELECT * FROM cadastro  WHERE cod=$cod";
+    $result7 = $conexao_forms15->query($sqlselect7);
 
     if($result->num_rows > 0)
     {
@@ -13,8 +31,44 @@
        $resultdelete = $conexao_forms15->query($sqldelete);
     }
 
+    if($result2->num_rows > 0)
+    {
+       $sqldelete="DELETE FROM saude_12_meses WHERE cod=$cod";
+       $resultdelete = $conexao_forms15->query($sqldelete);
+    }
+
+    if($result3->num_rows > 0)
+    {
+       $sqldelete="DELETE FROM relacionamento_12_meses WHERE cod=$cod";
+       $resultdelete = $conexao_forms15->query($sqldelete);
+    }
+
+    if($result4->num_rows > 0)
+    {
+       $sqldelete="DELETE FROM trabalho_12_meses WHERE cod=$cod";
+       $resultdelete = $conexao_forms15->query($sqldelete);
+    }
+
+    if($result5->num_rows > 0)
+    {
+       $sqldelete="DELETE FROM dinheiro_12_meses WHERE cod=$cod";
+       $resultdelete = $conexao_forms15->query($sqldelete);
+    }
+
+    if($result6->num_rows > 0)
+    {
+       $sqldelete="DELETE FROM outro_12_meses WHERE cod=$cod";
+       $resultdelete = $conexao_forms15->query($sqldelete);
+    }
+
+    if($result7->num_rows > 0)
+    {
+       $sqldelete="DELETE FROM cadastro WHERE cod=$cod";
+       $resultdelete = $conexao_forms15->query($sqldelete);
+    }
+
     //cadastro
-    $cod = $_GET['cod'];
+    /*$cod = $_GET['cod'];
     $sqlselect = "SELECT * FROM cadastro WHERE cod=$cod";
     $result2 = $conexao_regis->query($sqlselect);
 
@@ -28,7 +82,7 @@
 
        $sqldelete2="DELETE FROM formulario_15_anos WHERE cod=$cod";
        $resultdelete = $conexao_forms15->query($sqldelete);
-    }
+    }*/
   }
   
   header('Location: sistema.php');
